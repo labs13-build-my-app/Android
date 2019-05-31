e1bd76c3a4149b0e635a47fd854f3b94d85ca9f5
Pablo Ogata <pabloogata@gmail.com>
2019-05-31 15:04:35 -0400

    connecting drawer to bottom navigation

--- app/src/main/java/com/buildmyapp/android/BottomNavigationBehavior.java
+++ app/src/main/java/com/buildmyapp/android/BottomNavigationBehavior.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 
 public class BottomNavigationBehavior extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,25 @@ public class BottomNavigationBehavior extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+    }
+        return super.onOptionsItemSelected(item);
+}
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +60,9 @@ public class BottomNavigationBehavior extends AppCompatActivity {
                             selectedFragment = new SearchFragment();
                             break;
                         case R.id.navigationMyCourses:
+                            selectedFragment = new CoursesFragment();
+                            break;
+                        case R.id.navigationMenu:
                             selectedFragment = new CoursesFragment();
                             break;
                     }
