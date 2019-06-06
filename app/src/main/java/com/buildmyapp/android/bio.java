2a238ba950b4e6c34b0c17a2eb354a5eb3075b26
Pablo Ogata <pabloogata@gmail.com>
2019-06-06 17:45:30 -0400

    messeging done

--- app/src/main/java/com/buildmyapp/android/bio.java
+++ app/src/main/java/com/buildmyapp/android/bio.java
@@ -1,13 +1,29 @@
 package com.buildmyapp.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class bio extends AppCompatActivity {
+
+    CardView sendMessege;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bio);
+
+        sendMessege = (CardView) findViewById(R.id.message_cv);
+
+        sendMessege.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toMessege = new Intent(bio.this, MessageSeller.class);
+                startActivity(toMessege);
+            }
+        });
     }
 }
