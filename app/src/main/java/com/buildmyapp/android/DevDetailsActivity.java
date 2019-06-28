4a9b6588fef811636bf1ac84db88c90e9bef7651
Pablo Ogata <pabloogata@gmail.com>
2019-06-28 12:55:46 -0400

    owner and dev message

--- app/src/main/java/com/buildmyapp/android/DevDetailsActivity.java
+++ app/src/main/java/com/buildmyapp/android/DevDetailsActivity.java
@@ -1,11 +1,15 @@
 package com.buildmyapp.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.buildmyapp.android.innerActivity.MessageSeller;
 import com.bumptech.glide.Glide;
 
 public class DevDetailsActivity extends AppCompatActivity {
@@ -13,6 +17,7 @@ public class DevDetailsActivity extends AppCompatActivity {
 
     private ImageView profile;
     private AppCompatTextView name, desc;
+    private Button sendMessage;
 
 
     @Override
@@ -25,6 +30,7 @@ public class DevDetailsActivity extends AppCompatActivity {
         profile = findViewById(R.id.profile);
         name = findViewById(R.id.name);
         desc = findViewById(R.id.description);
+        sendMessage = findViewById(R.id.messageDev);
 
 
         desc.setText(getIntent().getStringExtra("desc"));
@@ -37,6 +43,15 @@ public class DevDetailsActivity extends AppCompatActivity {
 
 
         }
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent messageDev = new Intent(DevDetailsActivity.this, MessageSeller.class);
+                startActivity(messageDev);
+            }
+        });
 
     }
 
